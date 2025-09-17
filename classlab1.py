98ea4614f50c36184a6cfda3bd16fc13ae91345b
Mahi kr. <kumarmahi8758@gmail.com>
2025-09-18 02:38:00 +0530

    Class
    New

---
+++ classlab1.py
@@ -0,0 +1,14 @@
+x = int(input("Enter the number")) #assignment operator
+y = int(input("Enter the number")) #assignment operator
+z = input("input the symbol")#assignment operator
+if z == "+": #comparison operator
+    print(x+y)
+elif z == "-": #comparison operator
+    print(x-y)
+elif z== "*":#comparison operator
+    print(x*y)
+elif z =="/":#comparison operator
+    print(x/y)
+"""Arithematic Operation"""
+
+
