4f7cb6a32f762d8a8a7b1444a14c6eab1438aad0
Mahi kr <mahi@mahikr.kumarmahi8758@gmail.com>
2025-10-17 14:13:52 +0530

    python

--- Loop.py
+++ Loop.py
@@ -3,3 +3,8 @@ for name in names:
     print(name)
     print(len(name))
 print("loop is ended")
+sum = 0
+scores = [34, 56, 78, 90, 12]
+for score in scores:
+    sum = sum + score
+print("the total score is:", sum)
