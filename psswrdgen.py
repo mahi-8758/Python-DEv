35f684de204c26383ddbf29c12f4004368a53953
Mahi kr <mahi@mahikr.kumarmahi8758@gmail.com>
2025-10-17 14:56:35 +0530

    python

--- psswrdgen.py
+++ psswrdgen.py
@@ -1,21 +1,24 @@
-import random, string
-
-letters = string.ascii_lowercase
-caps = string.ascii_uppercase
-numbers = [str(i) for i in range(1, 11)]
-symbols = "!@#$%^&*()"
-
+lettercap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+symbol = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 print("Welcome to Password Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
 nr_lettercap = int(input("How many capital letters would you like?\n"))
-
-password = (
-    "".join(random.choice(letters) for _ in range(nr_letters)) +
-    "".join(random.choice(caps) for _ in range(nr_lettercap)) +
-    "".join(random.choice(numbers) for _ in range(nr_numbers)) +
-    "".join(random.choice(symbols) for _ in range(nr_symbols))
-)
-
-print("Your generated password is:", password)
+import random
+password = []
+for char in range(1, nr_letters + 1):        
+    password += random.choice(letter)                
+for char in range(1, nr_lettercap + 1):           
+    password += random.choice(lettercap)
+for num in range(1, nr_numbers + 1):
+    password += str(random.choice(number))
+for sym in range(1, nr_symbols + 1):
+    password += random.choice(symbol)
+random.shuffle(password)
+final_password = ""
+for char in password:
+    final_password += char
+print(f"Your password is: {final_password}")
