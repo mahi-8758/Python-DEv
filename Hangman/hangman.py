545e0e942149745fcaf11bdcf2300b0bdbe3b7b8
mahi-8758 <kumarmahi8758@gmail.com>
2025-10-20 02:33:34 +0530

    Update new

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,6 +1,3 @@
-from english_words import get_english_words_set
-
-# Get a set of English words from the "web2" list, lowercased
-web2_lowerset = get_english_words_set(['web2'], lower=True)
-print(f"Number of words in web2_lowerset: {len(web2_lowerset)}")
-print(list(web2_lowerset)[:1]) # Print first 10 words
+import input_word
+word = input_word.guess_word
+print(word)
---
+++ Hangman/input_word.py
@@ -0,0 +1,4 @@
+from english_words import get_english_words_set
+
+web2_lowerset = get_english_words_set(['web2'], lower=True)
+guess_word = (list(web2_lowerset)[:1])  
