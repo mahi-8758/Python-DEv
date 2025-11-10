5035d113c89c175dd9730516a1e62b9952dba60c
mahi-8758 <kumarmahi8758@gmail.com>
2025-11-10 17:50:38 +0530

    Gemini-Model

---
+++ Gemini-Model/main.py
@@ -0,0 +1,21 @@
+import google.generativeai as genai
+import api  
+
+api_keyai = api.API_KEY
+
+genai.configure(api_key=api_keyai)
+
+model = genai.GenerativeModel("gemini-2.0-flash")
+chat = model.start_chat()
+
+print ("Welcome to the Quiz AI-GOOGLE! Type 'exit' to quit.")
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        break
+    response = chat.send_message(user_input)
+    print("\n")
+    print("AI:", response.text)
+
+ 
